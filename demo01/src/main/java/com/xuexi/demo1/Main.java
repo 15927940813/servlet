eec9187156b688bfd34fb46bab7d202482813ab5
fengchenai <1171982294@qq.com>
2024-12-17 15:35:09 +0800

    push test2

--- demo01/src/main/java/com/xuexi/demo1/Main.java
+++ demo01/src/main/java/com/xuexi/demo1/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("master test");
         System.out.println("hotfix test");
         System.out.println("push test");
+        System.out.println("push test2");
     }
 }
