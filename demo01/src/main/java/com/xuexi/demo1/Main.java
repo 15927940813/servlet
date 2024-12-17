a74f9a901a55d24b7ec046958eaa3021dc7b6086
fengchenai <1171982294@qq.com>
2024-12-17 14:47:33 +0800

    test5

--- demo01/src/main/java/com/xuexi/demo1/Main.java
+++ demo01/src/main/java/com/xuexi/demo1/Main.java
@@ -3,6 +3,7 @@ package com.xuexi.demo1;
 public class Main {
     public static void main(String[] args) {
         String aa="111111";
+        String bb="222222";
         System.out.println("Hello world!");
     }
 }
